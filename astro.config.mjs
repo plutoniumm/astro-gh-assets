b19b176c5b954b40294a2730a8940fdb1ad7cf79
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-04-14 03:17:36 +0530

    reproduction deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig( {
+  output: 'static',
+  build: {
+    format: 'directory',
+  },
+  base: '/',
+  vite: { ssr: Object.create( null ) },
+  markdown: {},
+  site: 'https://demo.nukes.in',
+  server: {
+    port: 3000
+  },
+  base: '/',
+  integrations: [ svelte() ]
+} );
---
+++ src/components/Card.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let title = "Card Title";
+</script>
+
+<div>
+	<h5>{title}</h5>
+	<slot />
+</div>
+
+<style>
+</style>
